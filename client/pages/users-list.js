4dac6f2764990c8e87374f0157783bea5d63d737
Julio Makdisse Saito <saitodisse@gmail.com>
2014-08-20 18:39:03 -0300

    voltando para o caso anterior

--- client/pages/users-list.js
+++ client/pages/users-list.js
@@ -2,7 +2,6 @@
 var View = require('ampersand-view');
 var templates = require('../templates');
 var UserView = require('../views/user');
-var CollectionView = require('ampersand-collection-view');
 
 module.exports = View.extend({
 	template: templates.pages.users_list,
@@ -10,28 +9,11 @@ module.exports = View.extend({
 	autoRender: true,
 	
 	initialize: function() {
-		setTimeout(function() {
-			this.collection.fetch();
-		}.bind(this), 2000);
-		//this.collection.fetch();
+		this.collection.fetch();
 	},
 
-	subviews: {
-		users:{
-			waitFor: 'collection',
-			role: 'users-list',
-			prepareView: function() {
-				return new CollectionView({
-					el: this.el,
-					collection: this.collection,
-					view: UserView
-				});
-			}
-		}
+	render: function() {
+		this.renderWithTemplate();
+		this.renderCollection(this.collection, UserView, this.getByRole('users-list'));
 	}
-
-	// render: function() {
-	// 	this.renderWithTemplate();
-	// 	this.renderCollection(this.collection, UserView, this.getByRole('users-list'));
-	// }
 });
